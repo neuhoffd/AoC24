9cd942328812611d81c90d326991bbfa41c7ae6c
Daniel Neuhoff <daniel.neuhoff@googlemail.com>
2024-12-03 17:59:37 +0100

    Day 1 Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,37 +1,60 @@
 //use core::fmt;
-use std::{fmt::Binary, fs, string, vec};
+use std::{collections::HashMap, fs, string};
 
 fn main() {
     assert_eq!(play(".\\test0.txt", true), 11);
     assert_eq!(play(".\\input.txt", true), 1970720);
 
-    assert_eq!(play(".\\test0.txt", false), 11);
-    assert_eq!(play(".\\input.txt", false), 1970720);
+    assert_eq!(play(".\\test0.txt", false), 31);
+    assert_eq!(play(".\\input.txt", false), 17191599);
 }
 
-fn play(path: &str, part1: bool) -> i64 {
-    let mut parsed: (Vec<i64>, Vec<i64>) = parse(
-        path,
-        part1,
-    );
-    parsed.0.sort();
-    parsed.1.sort();
+fn part1(parsed: (Vec<i64>, Vec<i64>)) -> i64 {
     let mut ans: i64 = 0;
     for i in 0..parsed.0.len() {
         ans += (parsed.0[i] - parsed.1[i]).abs()
     }
-    println!("Part {}: \nResult: {}",
-        if part1 { 1 } else { 2 },
-        ans,);
+    ans
+}
+
+fn part2(parsed: (Vec<i64>, Vec<i64>)) -> i64 {
+    let mut ans: i64 = 0;
+    let mut map: HashMap<i64, i64> = HashMap::new();
+    for x in parsed.1 {
+        *map.entry(x).or_default() += 1;
+    }
+    for v in parsed.0 {
+        ans += map.get(&v).unwrap_or_else(|| &0) * v;
+    }
+    ans
+}
+
+fn play(path: &str, ispart1: bool) -> i64 {
+    let mut parsed: (Vec<i64>, Vec<i64>) = parse(path, ispart1);
+    let mut ans = 0i64;
+    parsed.0.sort();
+    parsed.1.sort();
+    if ispart1 {
+        ans = part1(parsed);
+    } else {
+        ans = part2(parsed);
+    }
+    println!("Part {}: \nResult: {}", if ispart1 { 1 } else { 2 }, ans,);
     ans
 }
 
 fn parse(path: &str, _part1: bool) -> (Vec<i64>, Vec<i64>) {
     let input = &fs::read_to_string(path)
-            .map(string::String::into_boxed_str)
-            .unwrap();
-    input.lines().map(|line| -> (i64, i64) {
-        let mut splits = line.split_whitespace();
-        (splits.next().unwrap().parse::<i64>().unwrap(), splits.last().unwrap().parse::<i64>().unwrap())
-    }).unzip()
+        .map(string::String::into_boxed_str)
+        .unwrap();
+    input
+        .lines()
+        .map(|line| -> (i64, i64) {
+            let mut splits = line.split_whitespace();
+            (
+                splits.next().unwrap().parse::<i64>().unwrap(),
+                splits.last().unwrap().parse::<i64>().unwrap(),
+            )
+        })
+        .unzip()
 }
