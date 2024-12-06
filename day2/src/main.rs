ad4c52fa62ce504a5d7fd1c3c181a21354ae949b
Daniel Neuhoff <daniel.neuhoff@googlemail.com>
2024-12-06 16:32:18 +0100

    day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,40 +3,60 @@ use std::{collections::HashMap, fs, string};
 
 fn main() {
     assert_eq!(play(".\\test0.txt", true), 2);
-    assert_eq!(play(".\\input.txt", true), 1970720);
+    assert_eq!(play(".\\input.txt", true), 306);
 
-    assert_eq!(play(".\\test0.txt", false), 31);
+    assert_eq!(play(".\\test0.txt", false), 4);
     assert_eq!(play(".\\input.txt", false), 17191599);
 }
 
-fn part1(parsed: Vec<Vec<i64>>) -> i64 {
-    let mut ans: i64 = 0;
+fn is_safe(report: &Vec<i64>) -> i64 {
+    let increments = increments(report);
+    if increments
+        .iter()
+        .all(|incr| incr.signum() == increments[0].signum() && incr.abs() <= 3 && incr.abs() >= 1)
+    {
+        1
+    } else {
+        0
+    }
+}
 
-    parsed
-        .into_iter()
-        .map(|line| -> i64 {
-            let increments: Vec<i64> = line[0..line.len() - 1]
-                .iter()
-                .zip(&line[1..line.len()])
-                .map(|(a, b)| -> i64 { b - a })
-                .collect();
-            if increments
-                .iter()
-                .map(|incr| -> (i64, i64) { (*incr, incr.signum()) })
-                .all(|(incr, sign)| sign == increments[0].signum() && incr.abs() <= 3)
-            {
-                1
-            } else {
-                0
-            }
-        })
-        .sum()
+fn increments(report: &Vec<i64>) -> Vec<i64> {
+    report[0..report.len() - 1]
+        .iter()
+        .zip(&report[1..report.len()])
+        .map(|(a, b)| -> i64 { b - a })
+        .collect()
+}
+
+fn part1(parsed: Vec<Vec<i64>>) -> i64 {
+    parsed.into_iter().map(|report| is_safe(&report)).sum()
 }
 
 fn part2(parsed: Vec<Vec<i64>>) -> i64 {
-    let mut ans: i64 = 0;
-
-    ans
+    parsed
+        .into_iter()
+        .map(|report| -> i64 {
+            if is_safe(&report) > 0 {
+                println!("Report {:?} is safe", report);
+                return 1;
+            }
+            for remove in 0..report.len() {
+                let mut without_remove = report.clone();
+                without_remove.remove(remove);
+                println!("Remove {} \n {:?}", remove, without_remove);
+                if is_safe(&without_remove) > 0 {
+                    println!(
+                        "Report {:?} is safe due to removal of level # {}",
+                        report, remove
+                    );
+                    return 1;
+                }
+            }
+            println!("Report {:?} is unsafe", report);
+            0
+        })
+        .sum()
 }
 
 fn play(path: &str, ispart1: bool) -> i64 {
